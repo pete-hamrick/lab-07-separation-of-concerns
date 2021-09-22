b9edba521d61b8a81586acff5a8f1a43b468773c
Pete Hamrick <pete.hamrick@gmail.com>
2021-09-22 15:18:13 -0700

    fixed some eslint errors, wrote comments on get route in test file

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,30 +1,38 @@
-const pool = require("../lib/utils/pool");
-const twilio = require("twilio");
-const setup = require("../data/setup");
-const request = require("supertest");
-const app = require("../lib/app");
+const pool = require('../lib/utils/pool');
+// const twilio = require('twilio');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
 
-jest.mock("twilio", () => () => ({
+jest.mock('twilio', () => () => ({
     messages: {
         create: jest.fn(),
     },
 }));
 
-describe("03_separation-of-concerns-demo routes", () => {
+describe('03_separation-of-concerns-demo routes', () => {
     beforeEach(() => {
         return setup(pool);
     });
 
-    it("creates a new order in our database and sends a text message", () => {
+    it('creates a new order in our database and sends a text message', () => {
         return request(app)
-            .post("/api/v1/orders")
+            .post('/api/v1/orders')
             .send({ quantity: 10 })
             .then((res) => {
                 // expect(createMessage).toHaveBeenCalledTimes(1);
                 expect(res.body).toEqual({
-                    id: "1",
+                    id: '1',
                     quantity: 10,
                 });
             });
+    });
+    it('gets all orders', () => {
+        //navigate to localhost
+        //hits app.js > app.use
+        //hits controllers/orders.js
+        //create get here?
+        //call orderService static returnOrders sending a message saying here are all orders
+        //or use Order class, static return orders
     });
 });
--- data/setup.js
+++ data/setup.js
@@ -1,7 +1,7 @@
-const fs = require("fs").promises;
+const fs = require('fs').promises;
 
 module.exports = (pool) => {
     return fs
-        .readFile(`${__dirname}/../sql/setup.sql`, { encoding: "utf-8" })
+        .readFile(`${__dirname}/../sql/setup.sql`, { encoding: 'utf-8' })
         .then((sql) => pool.query(sql));
 };
